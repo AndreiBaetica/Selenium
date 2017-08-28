53b913254e313d9d44db6cd4f5739983a7f97ebe
AndreiBaetica <andrei.baetic@gmail.com>
2017-08-27 21:47:22 -0400

    changed site

--- Selenium 2/src/main/App.java
+++ Selenium 2/src/main/App.java
@@ -8,63 +8,80 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Iterator;
+import java.util.List;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
-
 public class App {
 
 	public static void main(String[] args) throws IOException {
-		// save pic to desktop, create JSON file with username and img src
+		// save pic to desktop, create JSON file with img src
 		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		JSONObject obj = new JSONObject();
+		JSONArray list = new JSONArray();
 		int x = 1;
-		driver.get("http://writingexercises.co.uk/random-images.php");
-		
-		for(int i=0; i<10; i++){
-			downloader(driver, obj, x);
+		driver.get("https://warthunder.com/en/media/screenshots/");
+		WebElement next = driver.findElement(By.className("next"));
+
+		downloader(driver, obj, x, list);
+		next.click();
+		downloader(driver, obj, x, list);
+		while (true) {
+			try {
+				next.click();
+				break;
+			} catch (StaleElementReferenceException e) {
+				next = driver.findElement(By.className("next"));
+			}
 		}
-		
-		FileWriter fw = new FileWriter(new File("C://Users//Admin//Desktop//Documents//JSON File", "randomImage"));
-		fw.write(obj.toString());
-		fw.close();
-		
+		downloader(driver, obj, x, list);
 
-	}
-
-	static void downloader(WebDriver driver, JSONObject obj, int x) throws IOException{
-		WebElement pic = driver.findElement(By.cssSelector("#imageout > img"));
-		String src = pic.getAttribute("src");
 		try {
-			obj.put("src", src);
+			obj.put("src", list);
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		URL url = new URL(src);
-		InputStream in = new BufferedInputStream(url.openStream());
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		byte[] buf = new byte[1024];
-		int n = 0;
-		while (-1!=(n=in.read(buf)))
-		{
-		   out.write(buf, 0, n);
+
+		FileWriter fw = new FileWriter(new File("C://Users//Admin//Desktop//Documents//JSON File", "image"));
+		fw.write(obj.toString());
+		fw.close();
+
+	}
+
+	static void downloader(WebDriver driver, JSONObject obj, int x, JSONArray list) throws IOException {
+		List<WebElement> screens = driver.findElements(By.tagName("pic"));
+		Iterator<WebElement> iter = screens.iterator();
+		while (iter.hasNext()) {
+			WebElement pic = iter.next();
+			String src = pic.getAttribute("src");
+			list.put(src);
+			URL url = new URL(src);
+			InputStream in = new BufferedInputStream(url.openStream());
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			byte[] buf = new byte[1024];
+			int n = 0;
+			while (-1 != (n = in.read(buf))) {
+				out.write(buf, 0, n);
+			}
+			out.close();
+			in.close();
+			byte[] response = out.toByteArray();
+			FileOutputStream fos = new FileOutputStream(
+					"C://Users//Admin//Desktop//Documents//Selenium Images//image " + x + ".jpg");
+			fos.write(response);
+			fos.close();
+			x++;
 		}
-		out.close();
-		in.close();
-		byte[] response = out.toByteArray();
-		FileOutputStream fos = new FileOutputStream("C://Users//Admin//Desktop//Documents//Selenium Images//image "+x+".jpg");
-		fos.write(response);
-		fos.close();
-		x++;
-		WebElement button = driver.findElement(By.className("button"));
-		button.click();
+
 	}
 }
